93e21d39d1f0f09a205ed2bcb1efc146243c9f03
Citckunal <kunal.g15@gmail.com>
2026-05-29 08:55:43 +0530

    Update SignUp.py

--- pages/SignUp.py
+++ pages/SignUp.py
@@ -14,7 +14,7 @@ ix)button
 import streamlit as st
 import pymongo
 import random
-conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+conn=pymongo.MongoClient("mongodb+srv://kunalg15_db_user:<db_password>@citcapp.mong26u.mongodb.net/?appName=CitcApp")
 mydb=conn["ojt2"]
 my=mydb["student"]
 
