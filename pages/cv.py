da7151535fccda2c6f97eb714812e3401ab4e539
Citckunal <kunal.g15@gmail.com>
2026-05-30 16:32:06 +0530

    Update cv.py

--- pages/cv.py
+++ pages/cv.py
@@ -4,10 +4,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 st.title("C.V A n a l y z e r")
 st.success("User CV")
-
+save_dir = "./uploaded_files"
 f1=st.file_uploader("Upload User CV")
 file_path = os.path.join(f1.name)
-with open(file_path, "wb") as f:
+with open(save_dir,file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 reader =PdfReader(f1)
 cv = "".join(page.extract_text() for page in reader.pages)
@@ -16,7 +16,7 @@ st.write(cv)
 st.success("Job Description")
 f2=st.file_uploader("Upload Job Description")
 file_path1 = os.path.join(f2.name)
-with open(file_path1, "wb") as fx:
+with open(save_dir,file_path1, "wb") as fx:
         fx.write(uploaded_file.getbuffer())
 reader1 = PdfReader(f2)
 jd = "".join(page.extract_text() for page in reader1.pages)
